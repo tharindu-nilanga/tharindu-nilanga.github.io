d93b6b71d6fd49c0e8f004eb507f3e887de67d20
Tharindu Nilanga <106900993+tharindu-nilanga@users.noreply.github.com>
2022-12-28 11:44:09 +0530

    Add files via upload

---
+++ music-player/data.js
@@ -0,0 +1,23 @@
+let songs = [
+    {
+        name: 'shiver',
+        path: 'music/shiver.mp3',
+        author: '',
+        cover: 'covers/shiver.jpg'
+        
+    },
+    {
+        name: 'alone',
+        path: 'music/alone.mp3',
+        author: '',
+        cover: 'covers/alone.jpg'
+        
+    },
+    {
+        name: 'wishYouBest',
+        path: 'music/wish-you-the-best.mp3',
+        author: '',
+        cover: 'covers/wish-you-the-best.jpg'
+        
+    }
+]
---
+++ music-player/index.js
@@ -0,0 +1,79 @@
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+const songName = document.querySelector('.name');
+const currentTime = document.querySelector('.current-time')
+const songDuration = document.querySelector('.song-duration');
+const seekBar = document.querySelector('.seek-bar');
+const playButton = document.querySelector('.play-button');
+const cover = document.querySelector('.cover');
+const item1 = document.querySelector('.i1');
+const item2 = document.querySelector('.i2');
+const item3 = document.querySelector('.i3');
+
+
+playButton.addEventListener('click', () => {
+    playButton.classList.toggle('pause');
+    
+    if(playButton.className.includes('pause')){
+        music.pause();
+    }else{
+        
+        music.play();
+    }
+    
+    
+})
+
+//setup music
+
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+    
+    songName.innerHTML = song.name;
+    cover.style.backgroundImage = `url('${song.cover}')`;
+    
+    currentTime.innerHTML='00:00';
+    seekBar.max = music.duration;
+    setTimeout(() => {
+        seekBar.max=music.duration;
+        songDuration.innerHTML = formatTime(music.duration);
+        
+    }, 300);
+    
+    
+}
+
+setMusic(0);
+
+//formating time in min and secs
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;
+}
+
+item1.addEventListener('click', () => {
+    setMusic(0);
+    if(!playButton.className.includes('pause')){playButton.classList.toggle('pause');}
+})
+item2.addEventListener('click', () => {
+    setMusic(1);
+    if(!playButton.className.includes('pause')){playButton.classList.toggle('pause');}
+})
+item3.addEventListener('click', () => {
+    setMusic(2);
+    if(!playButton.className.includes('pause')){playButton.classList.toggle('pause');}
+    
+})
+
