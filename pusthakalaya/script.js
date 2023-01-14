15d892262df66370229c22c214684c00a13bd8e5
tharindu0nilanga <106900993+tharindu-nilanga@users.noreply.github.com>
2023-01-14 22:46:14 +0530

    update

---
+++ pusthakalaya/script.js
@@ -0,0 +1,13 @@
+// const body = document.querySelector('body')
+// const container = document.querySelector('nav')
+// const doc = document.documentElement
+
+// function changeSibling(className){
+//     const element = document.querySelector(className);
+//     if(element.nextElementSibling == null){
+//         container.children[0].style.backgroundColor = 'var(--eggplant)'
+//     }else{
+//         element.nextElementSibling.style.backgroundColor = 'var(--eggplant)'
+
+//     }
+// }
