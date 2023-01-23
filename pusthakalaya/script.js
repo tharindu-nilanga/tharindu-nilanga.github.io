5955298ec2b86bc54c750a58138d1f709509187f
tharindu0nilanga <106900993+tharindu-nilanga@users.noreply.github.com>
2023-01-23 07:11:43 +0530

    update

--- pusthakalaya/script.js
+++ pusthakalaya/script.js
@@ -3,18 +3,19 @@ const banners = document.querySelector('.banner_scroll')
 const rightArrow = document.querySelector('.right_arrow')
 const leftArrow = document.querySelector('.left_arrow')
 
-banners.scroll(0,0)
-
 let scrollPosition = 0
 function updateSize(){
     let viewportWidth = window.innerWidth;
     return viewportWidth;
 }
 updateSize()
+banners.scroll([updateSize*2],0)
+
 function scrollLeft(){
     scrollPosition -= updateSize();
     banners.scroll(scrollPosition,0 )
 }
+
 leftArrow.addEventListener('click', scrollLeft)
 function scrollRight(){
     scrollPosition += updateSize();
