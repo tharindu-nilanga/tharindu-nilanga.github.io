bbbf54b83403ae86430cf00552773a60f7d29e8d
tharindu0nilanga <106900993+tharindu-nilanga@users.noreply.github.com>
2023-01-23 15:16:10 +0530

    big update success

--- pusthakalaya/script.js
+++ pusthakalaya/script.js
@@ -1,4 +1,7 @@
+// initial
 
+
+//banners
 const banners = document.querySelector('.banner_scroll')
 const rightArrow = document.querySelector('.right_arrow')
 const leftArrow = document.querySelector('.left_arrow')
@@ -29,14 +32,49 @@ function scrollRight(){
     banners.scroll(scrollPosition,0)
     console.log(scrollPosition);
 }
-function refresh(){
-    location.reload()
-}
+
 leftArrow.addEventListener('click', scrollLeft)
 rightArrow.addEventListener('click', scrollRight)
-window.addEventListener("resize", updateSize);       
+window.addEventListener("resize", updateSize);
+
+//search
+let searchBarToggleClicks = 0;
+
+const bodySection = document.querySelector('.body')
+const searchEnable = document.querySelector('.searchIcon')
+const searchBar = document.querySelector('.search2')
+const searchInput = document.querySelector('.searchInput')
+const searchDisable = document.querySelector('.searchIcon2')
+
+//search enable
+function enableSearchBar(){
+    searchBarToggleClicks += 1;
+    searchBar.classList.add('on')
+    searchInput.focus()
+}
+function diableSearchBar(){
+    if(searchBarToggleClicks == 1){
+        searchBar.classList.remove('on')
+        searchInput.removeAttribute('autofocus')
+        searchInput.value = ''
+        searchBarToggleClicks = 0
+    }
+}
+function incrementSearchBarToggleClicks(){
+    searchBarToggleClicks += 1;
+}
+if (searchBarToggleClicks == 2){
+    searchBar.classList.remove('on')
+    searchBarToggleClicks = 0
+}
+
+searchEnable.addEventListener('click',enableSearchBar)
+searchDisable.addEventListener('click', diableSearchBar)
+bodySection.addEventListener('click', diableSearchBar)
+// refresh
+function refresh(){
+    if(!searchBar.classList.contains('on')){
+    location.reload()}
+}
 window.addEventListener("resize", refresh);       
 
-console.log(updateSize()/100*91*3)
-console.log(scrollPosition)
-console.log(updateSize())
